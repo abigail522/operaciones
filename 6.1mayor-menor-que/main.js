75ed9c532d40bd4712f7e43a1664961a5f028574
Formacion <formacion@FTAULAS.ORG>
2021-05-27 13:22:09 +0200

    operadores

---
+++ 6.1mayor-menor-que/main.js
@@ -0,0 +1,23 @@
+const numero1= 50;
+const numero2 = "50";
+const numero3 = 70;
+
+/*operador con mayor que */
+console.log(numero1>numero3);
+console.log(numero3<numero1);
+
+console.log(numero1< numero3);
+console.log(numero3<numero1);
+
+console.log(numero1 == numero2);
+
+console.log(numero1 === numero2);
+
+console.log(numero1=== parseInt(numero2));
+
+const passswor = "admin";
+const compruebaTupassword = "Admin";
+ console.log( passswor != compruebaTupassword);
+
+ console.log(numero1 !==numero2);
+ console.log(numero1 != numero2);
---
+++ 6.2comparar-null-y-undefined/main.js
@@ -0,0 +1,9 @@
+let nombre;
+
+console.log(nombre);
+console.log(typeof nombre);
+
+let numero1 = null;
+
+console.log(numero1);
+console.log(typeof numero1);
